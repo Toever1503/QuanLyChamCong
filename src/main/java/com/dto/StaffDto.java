32548591110ce5c196b890272f371843484409a3
Toever1503 <Haunv1503@gmail.com>
2022-05-18 11:52:00 +0700

    complete staff crud + login

---
+++ src/main/java/com/dto/StaffDto.java
@@ -0,0 +1,44 @@
+package com.dto;
+
+import com.entity.Staff;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class StaffDto {
+    private Long staffId;
+
+    private String staffName;
+    private String email;
+    private String password;
+    private Date birthday;
+    private Double salary;
+    private String avatar;
+    private Date createdAt;
+    private String manager;
+    private String position;
+
+    public static StaffDto toDto(Staff entity) {
+        if (entity == null) throw new RuntimeException("Entity is null");
+        return StaffDto.builder()
+                .staffId(entity.getStaffId())
+                .staffName(entity.getStaffName())
+                .email(entity.getEmail())
+                .password(entity.getPassword())
+                .birthday(entity.getBirthday())
+                .salary(entity.getSalary())
+                .avatar(entity.getAvatar())
+                .createdAt(entity.getCreatedAt())
+                .manager(entity.getManager() != null ? entity.getManager().getStaffName() : null)
+                .position(entity.getPosition().getPositionName())
+                .build();
+    }
+
+}
