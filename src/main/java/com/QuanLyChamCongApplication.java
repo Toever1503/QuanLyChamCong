305996d399ea96988e4d79f09528213559bd7325
Toever1503 <Haunv1503@gmail.com>
2022-05-18 09:11:16 +0700

    Create Base

---
+++ src/main/java/com/QuanLyChamCongApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyChamCongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLyChamCongApplication.class, args);
+    }
+
+}
