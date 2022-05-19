9dba971d62fa7bcb0d669f62f2588032fc545376
vuivehieu <75554300+vuivehieu@users.noreply.github.com>
2022-05-19 21:22:02 +0700

    tinh luong
    tinh luong

---
+++ src/main/java/com/dto/SalaryDto.java
@@ -0,0 +1,29 @@
+package com.dto;
+
+import com.entity.Salary;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SalaryDto {
+    private Long id;
+    private int late_day;
+    private int off_day;
+    private int work_day;
+    private int ot_hour;
+    private int month;
+    private double total_salary;
+
+    public static SalaryDto toDto(Salary salary)
+    {
+        if(salary == null){
+            return null;
+        }else
+        return SalaryDto.builder().total_salary(salary.getTotal_salary()).id(salary.getId()).ot_hour(salary.getOt_hour()).off_day(salary.getOff_day()).work_day(salary.getWork_day()).late_day(salary.getLate_day()).month(salary.getMonth()).build();
+    }
+}
