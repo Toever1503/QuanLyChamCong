20e9bb9fb06c4d9a3dd0c08be0538ecea430275a
Toever1503 <Haunv1503@gmail.com>
2022-05-24 15:16:23 +0700

    fix method filter by date

--- src/main/java/com/web/RequestStatusResources.java
+++ src/main/java/com/web/RequestStatusResources.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("request-status")
+@RequestMapping("api/request-status")
 public class RequestStatusResources {
 
 
