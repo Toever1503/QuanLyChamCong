584bf074fd73f51405c1a03268c5ccaca95b5038
Đinh Thế Vũ <thevu091193@gmail.com>
2022-05-25 14:55:10 +0700

    update export file excel salaries of staff
    1. add dependence into file pom.xml
    2.add method findAllByStaffStaffIdInAndMonth into ISalaryRepository
    3.add method findAllStaffForManager into  IStaffRepository
    4.add method caculateTotalSalaryForEmployee into SalaryServiceImpl
    5.add method getExportAllSalariesOfStaff into SalaryResuorce

--- src/main/java/com/service/ISalaryService.java
+++ src/main/java/com/service/ISalaryService.java
@@ -6,10 +6,14 @@ import com.model.SalaryModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface ISalaryService extends IBaseService<Salary, SalaryModel,Long>{
     Salary calculateSalary(Long client_id);
 
     Page<Salary> calculateTotalSalaryForEmployee(Pageable page);
+
+    List<Salary> calculateTotalSalaryForEmployee();
 
     Salary getMySalaryByMonth(int month);
 
--- src/main/java/com/service/impl/SalaryServiceImp.java
+++ src/main/java/com/service/impl/SalaryServiceImp.java
@@ -129,6 +129,18 @@ public class SalaryServiceImp implements ISalaryService {
         return this.salaryRepository.findAllByStaffStaffIdInAndMonth(staffIds, currentMonth, page);
     }
 
+    @Override
+    public List<Salary> calculateTotalSalaryForEmployee() {
+        List<Long> staffIds = new ArrayList<>();
+        this.staffRepository.findAllStaffForManager(SecurityUtil.getCurrentUserId())
+                .forEach(staff -> {
+                    staffIds.add(staff.getStaffId());
+                    this.calculateSalary(staff.getStaffId());
+                });
+        int currentMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
+        return this.salaryRepository.findAllByStaffStaffIdInAndMonth(staffIds, currentMonth);
+    }
+
     //Tính lương nhân viên theo tháng // Calculate employee salary by month
     @Override
     public Salary getMySalaryByMonth(int month) {
